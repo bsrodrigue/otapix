3bd92b6b595214d66d895e6283e8d66f255527bf
BADINI Rachid Rodrigue <brodrigue@protonmail.com>
2022-08-18 12:09:51 +0000

    config: change README and fix api key exposing

--- config/firebase/core/index.ts
+++ config/firebase/core/index.ts
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAwYNGgseaqWq-cy0rSikmIu6XUWgZuVBg",
-    authDomain: "four-pics-anime.firebaseapp.com",
-    projectId: "four-pics-anime",
-    storageBucket: "four-pics-anime.appspot.com",
-    messagingSenderId: "631346840242",
-    appId: "1:631346840242:web:d1eabee4a90a9392a07fab"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
