00db69f62a9ac9b5c1e50be5914723e3930a3582
Rodrigue <brodrigue@protonmail.com>
2022-08-19 08:53:37 +0000

    feature: start implementing pack creation

--- api/firebase/index.ts
+++ api/firebase/index.ts
@@ -1,54 +1,60 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, User } from "firebase/auth";
-import { collection, DocumentData, getDocs, QueryDocumentSnapshot } from "firebase/firestore";
+import { collection, doc, DocumentData, getDocs, QueryDocumentSnapshot, setDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { auth } from "../../config/firebase/auth";
 import { db } from "../../config/firebase/firestore";
 import { storage } from "../../config/firebase/storage";
+import { HasID, PuzzlePack } from "../../types";
 
 const USER_PROFILES = "user_profiles";
 
 export interface RegisterRequestParams {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 };
 
 export async function signUp({ email, password }: RegisterRequestParams) {
-    const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-    return userCredentials;
+  const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+  return userCredentials;
 }
 
 export async function signIn({ email, password }: RegisterRequestParams) {
-    const userCredentials = await signInWithEmailAndPassword(auth, email, password);
-    return userCredentials;
+  const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+  return userCredentials;
 }
 
 export async function uploadFile(path: string, file: Blob) {
-    const fileRef = ref(storage, path);
-    const snapShop = await uploadBytes(fileRef, file);
-    const downloadURL = await getDownloadURL(snapShop.ref);
-    return downloadURL;
+  const fileRef = ref(storage, path);
+  const snapShop = await uploadBytes(fileRef, file);
+  const downloadURL = await getDownloadURL(snapShop.ref);
+  return downloadURL;
 }
 
 export async function uploadUserFile(fileName: string, file: Blob, user: User) {
-    const path = `user_data/user_${user.email}/${fileName}`;
-    const downloadURL = await uploadFile(path, file);
-    return downloadURL;
+  const path = `user_data/user_${user.email}/${fileName}`;
+  const downloadURL = await uploadFile(path, file);
+  return downloadURL;
 }
 
 export async function uploadProfilePicture(file: Blob, user: User) {
-    const fileName = `profile_picture.${file.type.replace("image/", "")}`;
-    const photoURL = await uploadUserFile(fileName, file, user);
-    await updateProfile(user, {
-        photoURL,
-    });
-    return photoURL;
+  const fileName = `profile_picture.${file.type.replace("image/", "")}`;
+  const photoURL = await uploadUserFile(fileName, file, user);
+  await updateProfile(user, {
+    photoURL,
+  });
+  return photoURL;
 }
 
 export async function getUserProfiles() {
-    const profiles: any[] = [];
-    const querySnapshot = await getDocs(collection(db, USER_PROFILES));
-    querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData>) => {
-        profiles.push(doc.data());
-    })
-    return profiles;
+  const profiles: any[] = [];
+  const querySnapshot = await getDocs(collection(db, USER_PROFILES));
+  querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData>) => {
+    profiles.push(doc.data());
+  })
+  return profiles;
+}
+
+export async function createPuzzlePack(pack: Extract<PuzzlePack, HasID>) {
+  const { cover, ...rest } = pack;
+  await setDoc(doc(db, "packs", "player"), { ...rest });
 }
--- ui/components/Editor/PackEditor/PackEditor.tsx
+++ ui/components/Editor/PackEditor/PackEditor.tsx
@@ -7,7 +7,9 @@ import { PuzzleGrid } from "../../Grid/PuzzleGrid";
 import { DifficultyRadioGroup } from "../../RadioGroup/DifficultyRadioGroup";
 import { EditorWrapper } from "../EditorWrapper";
 import { PuzzleEditor } from "../PuzzleEditor";
+import { useForm } from "react-hook-form"
 import style from "./PackEditor.module.css";
+import { createPuzzlePack } from "../../../../api/firebase";
 
 interface PackEditorProps {
   currentPack: PuzzlePack;
@@ -16,62 +18,84 @@ interface PackEditorProps {
 export default function PackEditor({ currentPack }: PackEditorProps) {
   const [checkedDifficulty, setCheckedDifficulty] = useState<any>(Difficulty.F);
   const [puzzleEditorIsOpen, setPuzzleEditorIsOpen] = useState<boolean>(false);
+  const { handleSubmit, register, setValue } = useForm();
+  register("difficulty");
 
   useEffect(() => {
     setCheckedDifficulty(currentPack.difficulty);
   }, [currentPack]);
 
+  useEffect(() => {
+    setValue("difficulty", checkedDifficulty);
+  }, [checkedDifficulty]);
+
+  useEffect(() => {
+    setValue("puzzles", currentPack.puzzles);
+  }, [currentPack.puzzles]);
+
   return (
     <EditorWrapper>
-      <p>Couverture</p>
-      <RectangularDropzone
-        label="Telecharger une image"
-        name="pack-cover"
-        src={currentPack.cover}
-      />
-      <p>Titre</p>
-      <input
-        type="text"
-        placeholder="Trouvez un titre pour votre pack..."
-        value={currentPack?.title}
-      />
-      <p>Difficulte</p>
-      <small>Veuillez choisir une difficulte pour votre pack</small>
-      <DifficultyRadioGroup
-        checkedDifficulty={checkedDifficulty}
-        setCheckedDifficulty={setCheckedDifficulty}
-      />
-      {puzzleEditorIsOpen ? (
-        <PuzzleEditor
-          isOpen={puzzleEditorIsOpen}
-          setIsOpen={setPuzzleEditorIsOpen}
+      <form id="pack-editor-form" onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+        const payLoad = {
+          title: data?.title,
+          cover: data?.cover,
+          difficulty: data?.difficulty,
+          puzzles: data?.puzzles,
+        };
+        payLoad && createPuzzlePack(payLoad);
+      })} >
+        <p>Couverture</p>
+        <RectangularDropzone
+          label="Telecharger une image"
+          src={currentPack.cover}
+          {...register("cover")}
         />
-      ) : (
-        <>
-          <p>Liste de puzzle</p>
-          <PuzzleGrid puzzles={currentPack.puzzles} />
-          <Button
-            onClick={() => {
-              setPuzzleEditorIsOpen(true);
-              window.scrollTo(0, document.body.scrollHeight);
+        <p>Titre</p>
+        <input
+          type="text"
+          placeholder="Trouvez un titre pour votre pack..."
+          value={currentPack?.title}
+          {...register("title")}
+        />
+        <p>Difficulte</p>
+        <small>Veuillez choisir une difficulte pour votre pack</small>
+        <DifficultyRadioGroup
+          checkedDifficulty={checkedDifficulty}
+          setCheckedDifficulty={setCheckedDifficulty}
+        />
+        {puzzleEditorIsOpen ? (
+          <PuzzleEditor
+            isOpen={puzzleEditorIsOpen}
+            setIsOpen={setPuzzleEditorIsOpen}
+          />
+        ) : (
+          <>
+            <p>Liste de puzzle</p>
+            <PuzzleGrid puzzles={currentPack.puzzles} />
+            <Button
+              onClick={() => {
+                setPuzzleEditorIsOpen(true);
+                window.scrollTo(0, document.body.scrollHeight);
+              }}
+            >
+              Ajouter un puzzle
+            </Button>
+          </>
+        )}
+        {!puzzleEditorIsOpen && (
+          <input
+            type="submit"
+            value="Sauvegarder"
+            style={{
+              marginTop: "0.5em",
+              backgroundColor: "white",
+              color: "black",
             }}
-          >
-            Ajouter un puzzle
-          </Button>
-        </>
-      )}
-      {!puzzleEditorIsOpen && (
-        <Button
-          style={{
-            marginTop: "0.5em",
-            backgroundColor: "white",
-            color: "black",
-          }}
-          className={`${style.action}`}
-        >
-          Sauvegarder le pack
-        </Button>
-      )}
+            className={`${style.action}`}
+          />
+        )}
+      </form >
     </EditorWrapper>
   );
 }
