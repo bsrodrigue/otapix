867baa1b9df26473a21eaaadc3ad30f891f75e0c
Rodrigue <brodrigue@protonmail.com>
2022-08-23 06:30:59 +0000

    feature: disable spinnerbutton on loading

--- ui/components/Button/SpinnerButton/SpinnerButton.tsx
+++ ui/components/Button/SpinnerButton/SpinnerButton.tsx
@@ -2,17 +2,18 @@ import { TailSpin } from "react-loader-spinner";
 import style from "./SpinnerButton.module.css";
 
 export interface SpinnerButton {
+  text?: string;
   isLoading?: boolean;
 }
 
-export default function SpinnerButton({ isLoading }: SpinnerButton) {
+export default function SpinnerButton({ text, isLoading }: SpinnerButton) {
 
   return (
     <button
       type="submit"
       disabled={isLoading}
       className={`${style.container} ${isLoading && style.loading}`}>
-      {isLoading ? <TailSpin color='white' /> : "Valider"}
+      {isLoading ? <TailSpin color='white' /> : text || "Valider"}
     </button>
   );
 }
--- ui/components/Form/AuthForm/AuthForm.tsx
+++ ui/components/Form/AuthForm/AuthForm.tsx
@@ -1,58 +1,57 @@
 import { ReactNode } from 'react';
 import { TailSpin } from 'react-loader-spinner';
+import { SpinnerButton } from '../../Button/SpinnerButton';
 import style from './AuthForm.module.css';
 
 export interface AuthFormProps {
-    id?: string;
-    title?: string;
-    comment?: string;
-    subComment?: string;
-    children?: ReactNode;
-    message?: string[];
-    alternative?: string[];
-    onSubmit?: (e: any) => void;
-    isLoading?: boolean;
+  id?: string;
+  title?: string;
+  comment?: string;
+  subComment?: string;
+  children?: ReactNode;
+  message?: string[];
+  alternative?: string[];
+  onSubmit?: (e: any) => void;
+  isLoading?: boolean;
 }
 
 export default function AuthfForm({
-    id,
-    title,
-    comment,
-    subComment,
-    children,
-    message,
-    alternative,
-    onSubmit,
-    isLoading,
+  id,
+  title,
+  comment,
+  subComment,
+  children,
+  message,
+  alternative,
+  onSubmit,
+  isLoading,
 }: AuthFormProps) {
 
-    return (
-        <form
-            id={id}
-            className={style.authform_container}
-            onSubmit={onSubmit}
-            method="POST"
-        >
-            <div className={style.authform_header}>
-                <h1>{title}</h1>
-                <p>{comment}</p>
-                <small>{subComment}</small>
-            </div>
-            <div className={style.authform_fields}>
-                {children}
-            </div>
-            <div className={style.authform_footer}>
-                <a href={message?.[1]}>
-                    <small className={style.authform_message}>{message?.[0]}</small>
-                </a>
-                <button type="submit" className={`${style.authform_submit} ${isLoading && style.loading}`}>
-                    {isLoading ? <TailSpin color='white' /> : "Valider"}
-                </button>
-                <a className={style.authform_alternative} href={alternative?.[2]}>
-                    {alternative?.[0]}<span className='primary-color'>&nbsp;{alternative?.[1]}</span>
-                </a>
-            </div>
-        </form>
-    );
+  return (
+    <form
+      id={id}
+      className={style.authform_container}
+      onSubmit={onSubmit}
+      method="POST"
+    >
+      <div className={style.authform_header}>
+        <h1>{title}</h1>
+        <p>{comment}</p>
+        <small>{subComment}</small>
+      </div>
+      <div className={style.authform_fields}>
+        {children}
+      </div>
+      <div className={style.authform_footer}>
+        <a href={message?.[1]}>
+          <small className={style.authform_message}>{message?.[0]}</small>
+        </a>
+        <SpinnerButton isLoading={isLoading} />
+        <a className={style.authform_alternative} href={alternative?.[2]}>
+          {alternative?.[0]}<span className='primary-color'>&nbsp;{alternative?.[1]}</span>
+        </a>
+      </div>
+    </form>
+  );
 
 }
